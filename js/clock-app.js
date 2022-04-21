f85751d89c28984942a4ff698c4865d88ddd1915
Katherine Ebel <kathyebel@me.com>
2022-04-21 14:24:30 -0400

    fetch date/time data and update ui with results

--- js/clock-app.js
+++ js/clock-app.js
@@ -1,4 +1,5 @@
 import {QuoteGenerator} from "./quote-generator.js";
+import {GEO_API_KEY} from "./geoapikey.js";
 
 export class ClockApp {
   #moreButton;
@@ -12,6 +13,7 @@ export class ClockApp {
     this.#quote = document.getElementById('quote');
     this.#quoteAttribution = document.querySelector('.attribution');
     this.#bindEvents();
+    ClockApp.#getDetails().then(details => this.#updateUI(details));
   }
 
   #bindEvents() {
@@ -27,8 +29,22 @@ export class ClockApp {
     })
   }
 
+
+  static async #getDetails() {
+    try {
+      let geoIP = await fetch(`https://api.freegeoip.app/json/?apikey=${GEO_API_KEY}`)
+      const {country_code, region_name, time_zone} = await geoIP.json();
+
+      const worldTime = await fetch(`http://worldtimeapi.org/api/timezone/${time_zone}`)
+      const {abbreviation, day_of_week, day_of_year, week_number, utc_datetime} = await worldTime.json();
+      return {abbreviation, country_code, day_of_week, day_of_year, region_name, time_zone, utc_datetime, week_number}
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
   #updateQuote(quote) {
-    const { author, quote: q} = quote;
+    const {author, quote: q} = quote;
     [this.#quote, this.#quoteAttribution].forEach(el => {
       el.classList.replace('fade-in', 'fade-out');
     })
@@ -39,5 +55,38 @@ export class ClockApp {
         el.classList.replace('fade-out', 'fade-in');
       })
     }, 300)
+  }
+
+  static #getTime(dateString) {
+    let date = new Date(dateString);
+    let hour = String(date.getHours()).padStart(2, '0')
+    let minutes = String(date.getMinutes()).padStart(2, '0')
+    return `${hour}<span class="blink">:</span>${minutes}`
+  }
+
+  #trackTime(date) {
+    let ms = date.getTime();
+    const oneMinute = 60 * 1000
+    setInterval(() => {
+      let newDate = new Date(oneMinute + ms);
+      ms = newDate.getTime();
+      document.getElementById('time').innerHTML = ClockApp.#getTime(newDate.toUTCString())
+    }, oneMinute)
+  }
+
+  #updateUI(details) {
+    const { utc_datetime} = details;
+    this.#trackTime(new Date(utc_datetime));
+    details.time = ClockApp.#getTime(utc_datetime);
+    delete details.utc_datetime;
+    Object.keys(details).forEach(key => {
+      const el = document.getElementById(key);
+      if (!el) return console.log('no element for', key)
+      if (key === 'time') {
+        el.innerHTML = details[key]
+      } else {
+        el.textContent = details[key];
+      }
+    })
   }
 }
